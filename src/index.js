891f3bf25cd3c7c5c1353a962a95b126a8283aa5
OlesyaBorodich <lesyaborodich@mail.ru>
2019-03-24 22:02:59 +0300

    feat:add task advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var num2 = 0, num5 = 0; 
+  while(number>1){
+    var b=number;
+      while(b%5==0){
+        b=b/5;
+        num5++;
+      }
+    number-=1;	
+  }
+return num5;
 }
