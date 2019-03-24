365610b0aececbfce37c6f61ab25a292c6ec3aa1
OlesyaBorodich <lesyaborodich@mail.ru>
2019-03-24 23:17:02 +0300

    feat:add task zero

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-module.exports = function getZerosCount(number, base) {
-  var num2 = 0, num5 = 0; 
-  while(number>1){
-    var b=number;
-      while(b%5==0){
-        b=b/5;
-        num5++;
+module.exports = function getZerosCount(number, base) 
+{
+  const factors = (n) =>
+   {
+      const all = [];
+      let del = 2;
+      while (n >= 2) {
+          if (n % del == 0) {
+              all.push(del);
+              n = n / del;
+          }
+          else {
+              del++;
+          }
       }
-    number-=1;	
+      const result = all.reduce(function (acc, el) {
+          acc[el] = (acc[el] || 0) + 1;
+          return acc;
+      }, {});
+      return result
   }
-return num5;
+  const power = (n, N) =>
+   {
+      var counter = 0;
+      for (var i = n; N / i >= 1; i *= n)
+          counter += Math.floor(N / i);
+      return counter;
+  }
+  var primes = factors(base);
+  var res;
+  for (key in primes)
+   {
+      if ((power(key, number) / primes[key]) < res || (!res)) {
+          res = power(key, number) / primes[key]
+      }
+  }
+  return Math.trunc(res)
 }
